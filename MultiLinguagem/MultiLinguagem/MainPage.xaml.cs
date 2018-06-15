c919851a1adde058158d5c920a30217553039aeb
Ricardo A. P. Prestes <ricardo@forlogic.net>
2018-06-15 14:05:20 -0300

    Subindo código

---
+++ MultiLinguagem/MultiLinguagem/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MultiLinguagem.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MultiLinguagem
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            lblWelcome.Text = AppResources.LabelTest;
+        }
+    }
+}
